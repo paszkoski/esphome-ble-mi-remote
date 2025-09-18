a47f397ef699785e7470eefb0333d60569aa6aa7
paszkoski <31516351+paszkoski@users.noreply.github.com>
2025-09-18 13:21:12 +0200

    Update button.cpp

--- components/ble_mi_remote/button.cpp
+++ components/ble_mi_remote/button.cpp
@@ -1,3 +1,4 @@
+
 #ifdef USE_ESP32
 
 #include "button.h"
@@ -9,11 +10,18 @@ namespace esphome {
 		static const char *const TAG = "ble_mi_remote";
 
 		void BleMiRemoteButton::press_action() {
+			if (is_pair_button_) {
+				// Press both special keys without timer to keep them held
+				parent_->pressSpecial((uint8_t) pair_key_a_, /*with_timer=*/false);
+				parent_->pressSpecial((uint8_t) pair_key_b_, /*with_timer=*/false);
+				// Schedule release after the pairing duration
+				this->set_timeout("pair_release", pair_duration_ms_, [this]() {
+					this->parent_->release();
+				});
+				return;
+			}
 			if (value_ > -1) {
 				parent_->pressSpecial((uint8_t) value_);
-			} else if (value_ == -2) {
-				// Special value for pairing button
-				parent_->startPairing();
 			} else {
 				parent_->release();
 			}
