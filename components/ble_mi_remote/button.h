29af430ba00973df6d0c64be42b3b712efffc9e5
paszkoski <31516351+paszkoski@users.noreply.github.com>
2025-09-18 13:21:24 +0200

    Update button.h

--- components/ble_mi_remote/button.h
+++ components/ble_mi_remote/button.h
@@ -15,10 +15,22 @@ namespace esphome {
 
 				void set_value(int8_t value) { value_ = value; }
 
+				void set_pair_keys(uint8_t first_key, uint8_t second_key) {
+					pair_key_a_ = first_key;
+					pair_key_b_ = second_key;
+					is_pair_button_ = true;
+				}
+
+				void set_pair_duration(uint32_t duration_ms) { pair_duration_ms_ = duration_ms; }
+
 			protected:
 				void press_action() override;
 
 				int8_t value_{-1};
+				bool is_pair_button_{false};
+				uint8_t pair_key_a_{0};
+				uint8_t pair_key_b_{0};
+				uint32_t pair_duration_ms_{10000};
 
 				BleMiRemote *parent_;
 		};
